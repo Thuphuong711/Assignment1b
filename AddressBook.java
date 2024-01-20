16cdd49098840f46e904449edd0aab827f51f41c
Dat <trandatnh@gmail.com>
2024-01-20 13:54:08 -0800

    Phuong implementation

--- AddressBook.java
+++ AddressBook.java
@@ -7,7 +7,7 @@
 public class AddressBook
 {
     private final Database database;
-    private final UI       ui;
+    private final UI ui;
 
     public AddressBook(final UI u)
     {
@@ -15,33 +15,109 @@ public class AddressBook
         database = new Database();
     }
 
+    /**
+     * reads a Person from the user interface 
+     * and adds them to the database
+     * 
+     */
     public void addPerson()
     {
+    	Person person = ui.readPerson();
+    	database.add(person);
     }
 
+    /**
+     * reads a Person’s name from the user interface 
+     * and tries to delete them 
+     */
     public void deletePerson()
     {
+    	String name = ui.readName();
+    	if (this.remove(name)) {
+    		ui.displayMsg(name + " was deleted successfully");
+    	} else {
+    		ui.displayErrorMsg("Could not delete " + name );
+    	}
     }
 
+    /**
+     * reads a Person’s name from the user interface 
+     * and tries to find them in the database. 
+     * If not found the error message “No such person” is displayed.
+     */
     public void findPerson()
     {
+    	String name = ui.readName();
+    	Person foundPerson = this.search(name);
+    	if (foundPerson == null) {
+    		ui.displayErrorMsg("There is no such person in the database");
+    	} else {
+    		this.display(foundPerson);
+    	}
+
     }
 
+    /**
+     * to check whether can remove or not, then remove
+     * @param name
+     * @return
+     */
     private boolean remove(final String name)
     {
         return (database.removeByName(name) != null);
     }
 
+    /**
+     * 
+     * @param name
+     * @return
+     */
     private Person search(final String name)
     {
         return (database.findByName(name));
     }
 
+    
+    /**
+     * displays all people in the database on the user interface
+     */
     public void displayAll()
     {
+    	Person[] storage = database.getAll();
+    	int len = storage.length;
+    	if (len == 0) {
+    		ui.displayErrorMsg("There is no record in the database");
+    	}
+    	else {
+    		System.out.printf("%-20s%-15s%n","Name","PhoneNumber");
+	    	for (int i = 0; i < len; i++) {
+	    		System.out.printf("%-20s%-15s%n", storage[i].getName(), storage[i].getPhoneNumber());
+	    		
+	    	}
+    	}
     }
 
+
+    /*I asked you about why this display() method has private access modifier.
+      I see the display() method in ConsoleUI class, but because this display() method in AddressBook class
+      is private, so I cannot call this method in ConsoleUI class */
+
+     /* Moreover, I don't really understand why we still need display() method in ConsoleUI class, for what purpose?
+     If we want to search the name of a person, we just need to call findPerson class via AddressBook object(named book),
+     I wonder why we still need display() and displayAll() method in ConsoleUI class as we can call these methods from AddressBook class,
+     I feel it like redundancy?/
+     
+    
+    /**
+     * displays the requested person on the user interface
+     * @param person
+     */
     private void display(final Person person)
     {
+    	System.out.printf("%-20s%-15s%n","Name","PhoneNumber");
+    	Person found = database.findByName(person.getName());
+    	String foundName = found.getName();
+    	String foundNumber = found.getPhoneNumber();
+    	System.out.printf("%-20s%-15s%n",foundName,foundNumber);
     }
 }
--- ConsoleUI.java
+++ ConsoleUI.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class ConsoleUI
+public class ConsoleUI implements UI
 {
     private final Scanner     input;
     private AddressBook addressBook;
@@ -10,6 +10,149 @@ public class ConsoleUI
     {
         input       = new Scanner(System.in);
     }
+    
+    //reads the users choice from a Scanner
+    public int readChoice(Scanner input)  {
+    	
+    	displayMsg("choice ?");
+    	int choice = 0;
+    	try {
+    		choice = input.nextInt();
+        	input.nextLine();
+    	} 
+    	
+    	catch(Exception e) {
+    		displayErrorMsg("Invalid input. Please type in number 1-5");
+    	}
+    	if (choice < 1 || choice > 5) {
+    		displayErrorMsg("Your choice is incorrect, please try again");
+    	}
+
+    	return choice;
+    	
+    }
+
+    
+    /**reads in the Person data from a Scanner 
+    and returns the new Person.
+    This method is used for addPerson method*/
+    
+	@Override
+	public Person readPerson() {
+		String name = "";
+		String phoneNumber = "";
+		
+		name = readName();
+		displayMsg("Enter person's phone");
+		phoneNumber = input.nextLine();
+		
+		Person person = new Person(name,phoneNumber);
+		return person;
+	}
+	
+	
+	/**reads in the name of a Person from a Scanner
+	This is used for findPerson method
+	*/
+	@Override
+	public String readName() {
+		String name = "";
+		displayMsg("Enter person's name");
+		name = input.nextLine();
+		return name;
+	}
+	
+	
+    //displays a single Person’s data
+	//display is private
+
+	//Question here:
+	// I don't understand why we need display() and displayAll() method in this ConsoleUI
+	// We have already had these two methods in AddressBook class?
+
+	// Idea 1: make display() method in AddressBook class has public access modifier
+	// @Override
+	// public void display(Person person) {
+	// 	// TODO Auto-generated method stub
+	// 	addressBook.display(person);
+	// }
+
+	// Idea2: instead of calling display() method in AddressBook class and change it to public access modifier
+	// I call findPerson method 
+	@Override
+	public void display(Person person) {
+		addressBook.findPerson();
+	}
+	
+
+	//displays all people in the database
+	// Question: why add parameter to this method
+	@Override
+	public void displayAll(Person[] people) {
+		// TODO Auto-generated method stub
+		addressBook.displayAll();
+	}
+	
+
+	//displays the available menu
+	//this mean using the displayMsg() method?
+	public void displayMenu() {	
+		displayMsg("1) Add");
+		displayMsg("2) Delete");
+		displayMsg("3) Search");
+		displayMsg("4) Display All");
+		displayMsg("5) Exit");
+	}
+	
+	//displays the String message 
+	//passed on the user interface
+	@Override
+	public void displayErrorMsg(String msg) {
+		// TODO Auto-generated method stub
+		System.out.println(msg);
+	}
+	
+	//displays the String message 
+	//passed on the user interface
+	@Override
+	public void displayMsg(String msg) {
+		// TODO Auto-generated method stub
+		System.out.println(msg);
+		
+	}
+	
+	
+	//performs the address book functions
+	@Override
+	public void run(AddressBook book) {
+		// TODO Auto-generated method stub
+		boolean check = true;
+		
+		int choice = 0;
+		do {
+			displayMenu();
+			choice = readChoice(input);
+			switch(choice) {
+				case 1:
+					book.addPerson();
+					break;
+				case 2:
+					book.deletePerson();
+					break;
+				case 3:
+					book.findPerson();
+					break;
+				case 4:
+					book.displayAll();
+					break;
+				default :
+					check = false;
+			}
+		} while (check);
+		
+	}
+			
+	
 
 }
 
